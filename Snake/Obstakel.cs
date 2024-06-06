b50990da20d26cd9418ca9d8cbea197109540b94
Konektiv7 <conectiv2@gmail.com>
2024-06-07 01:40:50 +0200

    pierwszy

---
+++ Snake/Obstakel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Obstakel
+{
+    public int Xpos { get; set; }
+    public int Ypos { get; set; }
+    public ConsoleColor SchermKleur { get; set; }
+    public string Karacter { get; set; }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.CursorVisible = false;
+
+        Obstakel obstakel = new Obstakel
+        {
+            Xpos = 10,
+            Ypos = 5,
+            SchermKleur = ConsoleColor.Red,
+            Karacter = "X"
+        };
+
+        DrawObstakel(obstakel);
+
+        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static void DrawObstakel(Obstakel obstakel)
+    {
+        Console.SetCursorPosition(obstakel.Xpos, obstakel.Ypos);
+        Console.ForegroundColor = obstakel.SchermKleur;
+        Console.Write(obstakel.Karacter);
+        Console.ResetColor();
+    }
+}
